4a04091ee38e0e0095e888585b00cc75f772dfa0
tomasferro-dev <147525548+tomasferro-dev@users.noreply.github.com>
2024-10-01 17:36:26 -0300

    Update Burbuja.java

--- src/main/java/org/example/Burbuja.java
+++ src/main/java/org/example/Burbuja.java
@@ -1,4 +1,26 @@
 package org.example;
 
 public class Burbuja {
+    public static void hello () {
+        System.out.println("Hola Burbuja");
+    }
+    public static void burbuja(int array[]) {
+        int n = array.length;
+        boolean intercambiado;
+        do {
+            intercambiado = false;
+            for (int i = 1; i < n; i++){
+                if (array[i-1] > array[i]) {
+                    int aux = array[i-1];
+                    array[i-1] = array[i];
+                    array[i] = aux;
+                    intercambiado = true;
+                }
+            }
+        } while (intercambiado == true);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
 }
