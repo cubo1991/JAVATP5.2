40bb1322d0de6c1db0d4a628217ad5431d784bc7
tomasferro-dev <147525548+tomasferro-dev@users.noreply.github.com>
2024-10-01 17:36:50 -0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,17 @@
 package org.example;
 
 public class Main {
+    public static void burbuja(){
+        int[] array = {1,5,3,4,2};
+        Burbuja.burbuja(array);
+    }
     public static void insercion() {
         int[] array = {1,5,3,4,2};
         Insercion.insercion(array);
     }
 
     public static void main(String[] args) {
-        insercion();
+//        insercion();
+        burbuja();
     }
 }
